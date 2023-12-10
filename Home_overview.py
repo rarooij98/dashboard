e6437dab499e259f6c45f637a14402531eb1c802
Robin van Rooij <robinvanrooij@live.nl>
2023-12-10 23:49:51 +0100

    show columns

--- Home_overview.py
+++ Home_overview.py
@@ -103,10 +103,10 @@ if selected_df == "RDW":
     st.plotly_chart(fig)
 
 if selected_df == "Open Charge Map":
-    st.write(df_ocm.columns)
-    # st.write(df_ocm[['ID','OperatorID','UsageTypeID','UsageCost','Connections','NumberOfPoints','DateCreated','OperatorInfo.Title',
-    # 'UsageType.IsPayAtLocation','UsageType.IsMemberShipRequired','UsageType.IsAccessKeyRequired','UsageType.ID',
-    # 'UsageType.Title','StatusType.IsOperational','AdressInfo.AddressLine1','AdressInfo.Town','AdressInfo.StateOrProvince']].head())
+    # st.write(df_ocm.columns)
+    st.write(df_ocm[['ID','OperatorID','UsageTypeID','UsageCost','Connections','NumberOfPoints','DateCreated','OperatorInfo.Title',
+    'UsageType.IsPayAtLocation','UsageType.IsMemberShipRequired','UsageType.IsAccessKeyRequired','UsageType.ID',
+    'UsageType.Title','StatusType.IsOperational','AdressInfo.AddressLine1','AdressInfo.Town','AdressInfo.StateOrProvince']].head())
     
     fig = make_subplots(
         rows=5, cols=3,
