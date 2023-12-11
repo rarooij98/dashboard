cff7240f3c84bfedb3b4db578d52824eae184a1c
Robin van Rooij <robinvanrooij@live.nl>
2023-12-11 18:30:07 +0100

    add subplot with mean loading times

--- pages/2_Laadpaal_data.py
+++ pages/2_Laadpaal_data.py
@@ -4,6 +4,7 @@ import seaborn as sns
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+from plotly.subplots import make_subplots
 import statsmodels.formula.api as smf
 from statsmodels.tools import add_constant
 
@@ -48,13 +49,44 @@ ax.annotate(f"Median: {median:.0f}", xy=(median, 1400), xytext=(median+60, 1300)
 st.pyplot(fig_3)
 
 # New Figure
+# Filter the data
 filtered_df = df_lp[(df_lp['ChargeTime'] >= 0) & (df_lp['ConnectedTime'] >= 0)]
-fig = px.bar(filtered_df, x=filtered_df.index, y=['ConnectedTime', 'ChargeTime'],
-             title='Connected Time vs Charge Time per Charging Session (Filtered)',
-             labels={'value': 'Time (minutes)', 'variable': 'Type'},
-             barmode='stack')
-fig.update_xaxes(title='Charging Session')
-fig.update_yaxes(title='Time (minutes)')
+
+# Calculate the mean connected time and mean charging time
+mean_connected_time = filtered_df['ConnectedTime'].mean()
+mean_charge_time = filtered_df['ChargeTime'].mean()
+
+# Create a DataFrame for the mean values
+mean_df = pd.DataFrame({'Type': ['Connected Time', 'Charge Time'],
+                        'Mean Time': [mean_connected_time, mean_charge_time]})
+
+# Create the stacked bar chart
+fig1 = px.bar(filtered_df, x=filtered_df.index, y=['ConnectedTime', 'ChargeTime'],
+              title='Connected Time vs Charge Time per Charging Session (Filtered)',
+              labels={'value': 'Time (minutes)', 'variable': 'Type'},
+              barmode='stack')
+
+# Create a grouped bar chart for mean values
+fig2 = px.bar(mean_df, x='Type', y='Mean Time',
+              title='Mean Connected Time vs Mean Charge Time',
+              labels={'Mean Time': 'Time (minutes)'})
+
+# Create subplots
+fig = make_subplots(rows=1, cols=2,
+                    subplot_titles=['Connected Time vs Charge Time', 'Mean Connected Time vs Mean Charge Time'])
+
+# Add figures to subplots
+fig.add_trace(fig1.data[0], row=1, col=1)
+fig.add_trace(fig2.data[0], row=1, col=2)
+
+# Update layout
+fig.update_xaxes(title='Charging Session', row=1, col=1)
+fig.update_yaxes(title='Time (minutes)', row=1, col=1)
+
+fig.update_xaxes(title='Type', row=1, col=2)
+fig.update_yaxes(title='Time (minutes)', row=1, col=2)
+
+# Show the figure
 st.plotly_chart(fig)
 
 st.write('''
